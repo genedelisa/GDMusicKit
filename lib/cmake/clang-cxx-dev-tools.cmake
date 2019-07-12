007ca5d20e016e2a8b5b325aa7c00ae15351a052
Gene De Lisa <gene@rockhoppertech.com>
2019-07-12 10:21:43 -0400

    dev tools added to cmake

--- lib/cmake/clang-cxx-dev-tools.cmake
+++ lib/cmake/clang-cxx-dev-tools.cmake
@@ -1,37 +1,49 @@
-# how to use this:
-# Including custom cmake rules
-# include(cmake/clang-cxx-dev-tools.cmake)
+# Additional targets to perform clang-format and clang-tidy
 
-
-# Additional targets to perform clang-format/clang-tidy
-# Get all project files
+# Get all c++ files in lib
 file(GLOB_RECURSE
      ALL_CXX_SOURCE_FILES
-     *.[chi]pp *.[chi]xx *.cc *.hh *.ii *.[CHI]
-     )
+     RELATIVE "${PROJECT_SOURCE_DIR}/src"
+     *.[ch]pp 
+)
 
-# Adding clang-format target if executable is found
+# Add clang-format target if executable is found
 find_program(CLANG_FORMAT "clang-format")
 if(CLANG_FORMAT)
   add_custom_target(
     clang-format
-    COMMAND /usr/bin/clang-format
+    COMMAND ${CLANG_FORMAT}
     -i
     -style=file
     ${ALL_CXX_SOURCE_FILES}
     )
-endif()
+endif(CLANG_FORMAT)
 
-# Adding clang-tidy target if executable is found
+# Add clang-tidy target if executable is found
 find_program(CLANG_TIDY "clang-tidy")
+message("CLANG_TIDY: ${CLANG_TIDY}")
+message("ALL_CXX_SOURCE_FILES: ${ALL_CXX_SOURCE_FILES}")
+
 if(CLANG_TIDY)
   add_custom_target(
     clang-tidy
-    COMMAND /usr/bin/clang-tidy
+    COMMAND ${CLANG_TIDY}
+    -p ../build
+    -extra-arg=-std=c++17
     ${ALL_CXX_SOURCE_FILES}
-    -config=''
-    --
-    -std=c++11
-    ${INCLUDE_DIRECTORIES}
     )
-endif()
+endif(CLANG_TIDY)
+
+# I installed this via homebrew
+find_program(CPPCHECK "cppcheck")
+message("CPPCHECK: ${CPPCHECK}")
+if(CPPCHECK)
+add_custom_target(
+    cppcheck
+    COMMAND ${CPPCHECK}
+    --std=c++17 
+    --enable=all
+    ${ALL_CXX_SOURCE_FILES}
+    )
+endif(CPPCHECK)
+
